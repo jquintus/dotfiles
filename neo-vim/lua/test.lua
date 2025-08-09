9e01841fa7072750161b28fa92f1eaa7fe62eed6
Josh Quintus <josh.quintus@hightouch.io>
2025-08-09 12:15:17 -0400

    add comment.

--- neo-vim/lua/test.lua
+++ neo-vim/lua/test.lua
@@ -1,2 +1,6 @@
+-- This module is just here to quickly test that the module loading is working.
+-- It should always be the first module loaded. If there are issues on load, comment out the other modules and let this load.
+-- If this can't load, then there's something wrong with paths.
+
 print("Test module loaded: Hello World")
 vim.cmd("colorscheme murphy")
