35de822fd053e18b750d6f79cdeb8b9b98ea448b
Josh Quintus <josh.quintus@hightouch.io>
2025-08-10 13:01:57 -0400

    Getting structure built up

--- scripts/install-mac.sh
+++ scripts/install-mac.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Install NeoVim
-
+echo Install NeoVim Configs
 mkdir -p ~/.config/nvim
 rm ~/.config/nvim/init.lua
 ln -nfs ~/dotfiles/neo-vim/int.lua  ~/.config/nvim/init.lua
+
+echo Install gVim configs
+[ -f ~/.vimrc ] && rm ~/.vimrc
+ln -nfs ~/dotfiles/vim/init.vim ~/.vimrc
